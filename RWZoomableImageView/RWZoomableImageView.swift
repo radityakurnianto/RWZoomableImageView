a228bac26531effd38bff971f562011432d77e73
Raditya Kurnianto <raditya@detik.com>
2019-01-16 12:01:45 +0700

    zoom will be perform above UIWindow

--- RWZoomableImageView/RWZoomableImageView.swift
+++ RWZoomableImageView/RWZoomableImageView.swift
@@ -12,6 +12,23 @@ import UIKit
     
     var isZooming = false
     var originalImageCenter: CGPoint?
+    var originalRect: CGRect!
+    var originalPosition: CGPoint!
+    var firstCenterPoint: CGPoint!
+    
+    lazy var overlayView: UIView = { [unowned self] in
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.alpha = 0.5
+        view.backgroundColor = .black
+        return view
+        }()
+    
+    lazy var dummyImageView: UIImageView = { [unowned self] in
+        let imgView = UIImageView(frame: .zero)
+        imgView.contentMode = .scaleAspectFill
+        imgView.clipsToBounds = true
+        return imgView
+        }()
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,75 +47,57 @@ import UIKit
     
     fileprivate func attachGesture() -> Void {
         self.isUserInteractionEnabled = true
-        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.pinch(sender:)))
-        pinch.delegate = self
-        
-        let pan = UIPanGestureRecognizer(target: self, action: #selector(self.pan(sender:)))
-        pan.delegate = self
-        
-        self.addGestureRecognizer(pan)
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.pinchStateChanged(pinch:)))
         self.addGestureRecognizer(pinch)
     }
     
-    @objc fileprivate func pinch(sender: UIPinchGestureRecognizer) -> Void {
-        if sender.state == .began {
-            let currentScale = self.frame.size.width / self.bounds.size.width
-            let newScale = currentScale*sender.scale
-            if newScale > 1 {
-                self.isZooming = true
-            }
-        } else if sender.state == .changed {
-            guard let view = sender.view else {return}
-            let pinchCenter = CGPoint(x: sender.location(in: view).x - view.bounds.midX,
-                                      y: sender.location(in: view).y - view.bounds.midY)
-            let transform = view.transform.translatedBy(x: pinchCenter.x, y: pinchCenter.y)
-                .scaledBy(x: sender.scale, y: sender.scale)
-                .translatedBy(x: -pinchCenter.x, y: -pinchCenter.y)
-            let currentScale = self.frame.size.width / self.bounds.size.width
-            var newScale = currentScale*sender.scale
-            if newScale < 1 {
-                newScale = 1
-                let transform = CGAffineTransform(scaleX: newScale, y: newScale)
-                self.transform = transform
-                sender.scale = 1
-            }else {
-                view.transform = transform
-                sender.scale = 1
-            }
-        } else if sender.state == .ended || sender.state == .failed || sender.state == .cancelled {
-            guard let center = self.originalImageCenter else {return}
-            UIView.animate(withDuration: 0.3, animations: {
-                self.transform = CGAffineTransform.identity
-                self.center = center
-            }, completion: { _ in
-                self.isZooming = false
-            })
-        }
-    }
-    
-    @objc fileprivate func pan(sender: UIPanGestureRecognizer) -> Void {
-        if isZooming {
-            if sender.state == .began {
-                originalImageCenter = sender.view?.center
-            }
+    @objc fileprivate func pinchStateChanged(pinch: UIPinchGestureRecognizer) -> Void {
+        if pinch.state == .began {
+            self.isHidden = true
+            let window = UIApplication.shared.keyWindow
+            originalPosition = self.convert(self.frame.origin, to: nil)
+            originalRect = CGRect(origin: originalPosition, size: self.bounds.size)
+            firstCenterPoint = pinch.location(in: window)
             
-            if sender.state == .changed {
-                let translation = sender.translation(in: self)
-                if let view = sender.view {
-                    let x = view.center.x + translation.x
-                    let y = view.center.y + translation.y
-                    
-                    view.center = CGPoint(x: x, y: y)
-                }
-                
-                sender.setTranslation(.zero, in: self.superview)
+            dummyImageView.frame = originalRect
+            dummyImageView.contentMode = .scaleAspectFill
+            dummyImageView.clipsToBounds = true
+            dummyImageView.image = self.image
+            
+            window?.addSubview(overlayView)
+            window?.addSubview(dummyImageView)
+        }
+        
+        if pinch.state == .changed {
+            let currentScale = dummyImageView.frame.size.width / originalRect.size.width
+            let newScale = currentScale * pinch.scale
+            let newWidth = newScale * originalRect.size.width
+            let newHeight = newScale * originalRect.size.height
+            let newSize = CGSize(width: newWidth, height: newHeight)
+            
+            dummyImageView.frame = CGRect(origin: originalPosition, size: newSize)
+            
+            let currentWindow = UIApplication.shared.keyWindow
+            let centerXDif = firstCenterPoint.x - pinch.location(in: currentWindow).x
+            let centerYDif = firstCenterPoint.y - pinch.location(in: currentWindow).y
+            
+            let posX = originalRect.origin.x + (originalRect.size.width/2)-centerXDif
+            let posY = originalRect.origin.y + (originalRect.size.height/2)-centerYDif
+            dummyImageView.center = CGPoint(x: posX, y: posY)
+            pinch.scale = 1
+        }
+        
+        if pinch.state == .ended || pinch.state == .failed || pinch.state == .cancelled {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.dummyImageView.frame = self.originalRect
+            }) { (completed) in
+                self.originalPosition = .zero
+                self.originalRect = .zero
+                self.originalImageCenter = .zero
+                self.dummyImageView.removeFromSuperview()
+                self.overlayView.removeFromSuperview()
+                self.isHidden = false
             }
         }
-    }
-}
-
-extension RWZoomableImageView: UIGestureRecognizerDelegate {
-    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
     }
 }
